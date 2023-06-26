4728adc196a083e5609f76424d65e4c5d193758b
Ilya <iliaabr5@gmail.com>
2023-06-26 19:28:24 +0300

    initial

---
+++ src/main/java/com/example/digitalchieftesttask/DigitalChiefTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalchieftesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalChiefTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalChiefTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitalchieftesttask/DigitalChiefTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalchieftesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
